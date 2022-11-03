e40a469d7ddb24145dc5adc91489e71d6cb29e77
Jack Nolan <jnolan@recurforever.com>
2022-11-02 22:39:30 -0500

    Added prettier

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const isProd = process.env.NODE_ENV === "production";
 
@@ -8,11 +8,10 @@ const app = express();
 
 // CORS policy
 const origin = isProd ? process.env.WEBSITE_URL : "http://localhost:8080";
-app.use(cors({origin}));
+app.use(cors({ origin }));
 
-app.get('/ping', (req, res) => {
-  res.send('pong');
+app.get("/ping", (req, res) => {
+  res.send("pong");
 });
-
 
 app.listen(process.env.PORT || 5000);
