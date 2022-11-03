6768b9ea02eeb27395be065710f258ea6493f9b9
Jack Nolan <jnolan@recurforever.com>
2022-11-02 22:30:05 -0500

    init repo with basic scaffold

---
+++ README.md
@@ -0,0 +1,15 @@
+# Vennt Backend (TS)
+
+A rebuild of the original Vennt backend server in TS with a Mongo database
+
+Requirements:
+`node, pnpm`
+
+Start dev server:
+`pnpm run dev`
+
+Production build:
+`pnpm run build`
+
+Start server:
+`pnpm run start`
---
+++ index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+
+const isProd = process.env.NODE_ENV === "production";
+
+// INIT EXPRESS
+const app = express();
+
+// CORS policy
+const origin = isProd ? process.env.WEBSITE_URL : "http://localhost:8080";
+app.use(cors({origin}));
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+
+app.listen(process.env.PORT || 5000);
