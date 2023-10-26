fc1b5e67eed0cb94d8dbd1e29a23bda0f8d8a163
Jack Nolan <921Jack@gmail.com>
2023-10-26 11:20:08 -0500

    Spell Hand abilities

--- src/webscraper/abilitiesUses/paths/magus.ts
+++ src/webscraper/abilitiesUses/paths/magus.ts
@@ -46,6 +46,50 @@ export const MAGUS_USES: Record<string, UsesMap> = {
       },
     ],
   },
+  "Spell Hand I": {
+    criteria_benefits: [
+      {
+        criteria: {
+          type: "comp",
+          left: { type: "attr", attr: "free_hands" },
+          right: { type: "const", const: "2" },
+          operator: "gte",
+        },
+        adjust: { time: "permanent", attr: { casting: 1 } },
+        check: {
+          attr: "casting",
+          bonus: "+1",
+          label: "When at least 3m from all allies",
+        },
+      },
+    ],
+  },
+  "Spell Hand II": {
+    criteria_benefits: [
+      {
+        criteria: {
+          type: "comp",
+          left: { type: "attr", attr: "free_hands" },
+          right: { type: "const", const: "2" },
+          operator: "gte",
+        },
+        adjust: { time: "permanent", attr: { casting: 1 } },
+      },
+    ],
+  },
+  "Spell Hand III": {
+    criteria_benefits: [
+      {
+        criteria: {
+          type: "comp",
+          left: { type: "attr", attr: "free_hands" },
+          right: { type: "const", const: "2" },
+          operator: "gte",
+        },
+        adjust: { time: "permanent", attr: { casting: 1 } },
+      },
+    ],
+  },
   "Confidence of the Magus": {
     optional_heal: [
       {
